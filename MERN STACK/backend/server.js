9f6dc2aa8b1639adb789809fe3c25fe994079822
MOH. RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-05-27 09:46:25 +0700

    Add files via upload

---
+++ MERN STACK/backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+//t2OxubWU6r6ezT6Z
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+
+
+// listen for request
+app.listen(process.env.PORT, () => {
+    console.log('listening to port', process.env.PORT)
+}) 
+
+process.env
